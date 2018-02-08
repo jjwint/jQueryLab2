d7b5a6da7eb9659ffebe0db9012f5d99b72865a3
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-02-07 20:28:40 -0500

    added explanatory comments to js file

--- script.js
+++ script.js
@@ -4,40 +4,51 @@ $(document).ready(function(){
 	var nameInput = document.getElementById("name");
 	var userName = nameInput.value;
 
+//hides hidden reservation form on page load
+	$("#hiddenForm").hide();
+
+//sets the individual table currently being clicked on to a variable "table"
 	$(".tables").click(function() {
 		table = $(this);
 	});
 
+//sets the individual table currently being hovered over to "tableHover"
 	$(".tables").hover(function() {
 		tableHover = $(this);
 	});
 
-	$(".available").click(function() {
-		$("#hiddenForm").removeClass("hidden");
-		$("#hiddenForm").slideDown();
+//reveals the reservation form for a table that has class of "available"
+	$(".tables").click(function() {
+		if (table.hasClass("available")) {
+			$("#hiddenForm").removeClass("hidden");
+			$("#hiddenForm").slideDown(600);
+		}
 	});
 
-	$(".reserved").hover(function() {
-		$(".hoverTableInfo").css("display", "block");
-	});
-
+//retrieves the table number from the individual table's container div, and attaches 
+//that number to the reservation form for that particular table
 	$(".tables").click(function() {
 		var digit = $(this).text();
 		$("#tableDigit").text(digit);
 	});
 
+//when the user clicks the save button, the reservation form slides up and out of view
 	$("#saveButton").click(function() {
 		$(table).addClass("reserved");
 		$(table).removeClass("available");
 		$(table).addClass("notAllowed");
-		$("#hiddenForm").addClass("hidden");
+		$("#hiddenForm").slideUp(600);
 		retrieveTableData();
 	});
 
+//when the user x's out of the reservation form, it slides up and out of view
 	$("#quitButton").click(function() {
-		$("#hiddenForm").addClass("hidden");
+		$("#hiddenForm").slideUp(600);
 	});
 
+//if a table currently being hovered over has a class of "available", the background
+//color of the table will change to match the red heading, and will change back to grey 
+//when not being hovered over
 	$(".tables").hover(function() {
 		if (tableHover.hasClass("available")) {
 			$(tableHover).css("background-color", "#E34E45");
@@ -46,6 +57,8 @@ $(document).ready(function(){
 		$(tableHover).css("background-color", "");
 	});
 
+//if the table currently being hovered over has a class of "available", the cursor will 
+//change to a crosshair cursor, and changes back to default when not being hovered over
 	$(".tables").hover(function() {
 		if (tableHover.hasClass("available")) {
 			$(tableHover).css("cursor", "crosshair");
@@ -54,6 +67,8 @@ $(document).ready(function(){
 		$(tableHover).css("cursor", "");
 	});
 
+//retreives the name and party size inputs from reservation form; 
+//appends them to the table in a child div
 	function retrieveTableData() {
 		var name = $("#name").val();
 		var partySize = $("#partySize").val();
